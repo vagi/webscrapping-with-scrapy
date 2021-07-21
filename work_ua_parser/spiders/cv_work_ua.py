dfa5da76172de3aa4fbfe305ff197ea8f0c87de6
Vagif Molla <vagif69@gmail.com>
2021-07-22 01:30:27 +0300

    Minor changes to spider config

--- work_ua_parser/spiders/cv_work_ua.py
+++ work_ua_parser/spiders/cv_work_ua.py
@@ -1,10 +1,13 @@
 import scrapy
+from protego import Protego
 
 
 class CvWorkUaSpider(scrapy.Spider):
     name = 'cv_work_ua'
     allowed_domains = ['work.ua']
-    start_urls = ['http://work.ua/']
+    start_urls = ['https://www.work.ua/resumes-kharkiv/']
 
     def parse(self, response):
-        pass
+        for item in response.css('div#pjax-resume-list div.card.resume-link'):
+            print(item.css('h2 a::title').get())
+        

0a7ff73b82945be19f529c1b9524996ed9159e67
Vagif Molla <vagif69@gmail.com>
2021-07-22 14:43:56 +0300

    Implement parsing of name, age, salary and candidacy's skills description

--- work_ua_parser/spiders/cv_work_ua.py
+++ work_ua_parser/spiders/cv_work_ua.py
@@ -10,17 +10,34 @@ class CvWorkUaSpider(scrapy.Spider):
     def parse(self, response):
         for item in response.css('div#pjax-resume-list div.card.resume-link'):
 
+            card_details_uri = item.css('h2 > a::attr(href)').get()
+
             result = {
                 'position': item.css('h2 > a::text').get(),
                 'name': item.css('div > b::text').get(),
-                'age': item.css('div > span:nth-child(4)::text').get(),
+                # remove chars in age /'age': '18\xa0років'/ or process it before writing in csv file
+                'age': item.css('div span:nth-child(4)::text').get(),
+                'link': card_details_uri
 
             }
 
+            # Remove salary once test is completed
             salary = item.css('h2 span.nowrap::text').get()
-
             if salary:
                 result['salary'] = float(salary.replace(' грн', ''))
 
-            yield result
+            yield response.follow(card_details_uri, self.parse_card_details, meta={
+                'result': result
+            })
+
+    def parse_card_details(self, response):
+
+        header = response.css('div.card > h2::text').get()
+
+        description = ' '.join(response.css('div.card > p::text').getall())
+        description = ' '.join(description.split())
+
+        response.meta['result']['description'] = header + ': ' + description
+
+        yield response.meta['result']
         
